65fdc4ed082cb97a9277f006a29c065c172be2f7
juan.sanin <juan.sanin81694@u.icesi.edu.co>
2024-10-15 14:21:06 -0500

    fix: correct accept changes on new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 
-<<<<<<< HEAD
-=======
 import NavBar from './components/NavBar';
 
->>>>>>> 1231919 (feat: add navbar and button components)
 function App() {
 
 
   return (
     <>
-<<<<<<< HEAD
-      <h1>Welcome to CoursePlace</h1>
-=======
       <NavBar />
->>>>>>> 1231919 (feat: add navbar and button components)
     </>
   );
 }
