e38df85d02d51e17523c32c7e37d98538bcfa202
JCamiloRamirezTabares <juancaram21@gmail.com>
2024-11-17 14:44:06 -0500

    feat: add constants - added constants to represent types and default values

---
+++ src/consts/consts.d.tsx
@@ -0,0 +1,48 @@
+import { Program, World } from "./types";
+
+export const defaultPrograms: Program[] = [
+    {
+        id: "3e6bdc69-7f79-4545-a1e6-dcc65cd0f6a1",
+        name: "Maestría en Gerencia de Proyectos",
+        description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
+    },
+    {
+        id: "6599605e-cb4f-498e-82cd-79b2941bf513",
+        name: "Maestría en Ciencia de Datos",
+        description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
+    },
+    {
+        id: "b689fa3c-4237-4db2-9122-992c09d75662",
+        name: "Maestría en Experiencias Digitales",
+        description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
+    },
+    {
+        id: "408b590f-253b-4046-8970-49687573d4be",
+        name: "Maestría en Ciberseguridad",
+        description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
+    }
+];
+
+
+export const defaultWorlds: World[] = [
+    {
+        id: "3e6bdc69-7f79-4545-a1e6-dcc65cd0f6a1",
+        name: "Gerencia de Proyectos",
+        description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
+    },
+    {
+        id: "6599605e-cb4f-498e-82cd-79b2941bf513",
+        name: "Inteligencia Artificial",
+        description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
+    },
+    {
+        id: "b689fa3c-4237-4db2-9122-992c09d75662",
+        name: "Liderazgo",
+        description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
+    },
+    {
+        id: "408b590f-253b-4046-8970-49687573d4be",
+        name: "Ciencia de Datos",
+        description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
+    },
+];
