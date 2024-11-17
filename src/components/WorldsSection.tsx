3818e023a027afad1cca6b57eb198cc83e89ec55
JCamiloRamirezTabares <juancaram21@gmail.com>
2024-11-17 14:50:02 -0500

    feat: integrate ProgramsSection and WorldsSection - integrated components with default values

--- src/components/WorldsSection.tsx
+++ src/components/WorldsSection.tsx
@@ -3,40 +3,10 @@ import WorldCard from './WorldCard';
 import HeroTitle from './HeroTitle';
 import aiImage from '../assets/ai.jpg';
 import Button from './Button';
-
-type UUID = `${string}-${string}-${string}-${string}-${string}`;
-
-interface World {
-  id: UUID,
-  name: string,
-  description: string
-}
-
+import { defaultWorlds } from '../consts/consts.d';
 
 const WorldsSection: React.FC = () => {
 
-  const defaultWorlds: World[] = [
-    {
-      id: "3e6bdc69-7f79-4545-a1e6-dcc65cd0f6a1",
-      name: "Gerencia de Proyectos",
-      description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
-    },
-    {
-      id: "6599605e-cb4f-498e-82cd-79b2941bf513",
-      name: "Inteligencia Artificial",
-      description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
-    },
-    {
-      id: "b689fa3c-4237-4db2-9122-992c09d75662",
-      name: "Liderazgo",
-      description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
-    },
-    {
-      id: "408b590f-253b-4046-8970-49687573d4be",
-      name: "Ciencia de Datos",
-      description: "Descripción del mundo, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
-    },
-  ];
 
   return (
     <div className="px-16 py-16 bg-white text-center flex flex-col">
