0fe906eb723e8669ff5a5e81e7c0c2a2f016a7ec
juan.sanin <juan.sanin81694@u.icesi.edu.co>
2024-11-19 16:18:10 -0500

    feat: Talent Cycle component and Searchbar implemented

--- src/components/TalenDevSection.tsx
+++ src/components/TalenDevSection.tsx
@@ -1,73 +1,84 @@
 import React from "react";
 import CourseCard from "./CourseCard";
-import PostGradCard from "./PostGradCard";
+
+import TalentSearchBar from "./TalentSearchBar";
+import TalentCycleComponent from "./TalentCycleComponent";
 
 const TalentDevSection: React.FC = () => {
     return (
-        <div className="flex flex-col gap-6 p-6 bg-white">
-            <div className="flex flex-row gap-[1rem]">
-            <CourseCard
-                title="Card Curso"
-                description="Descripción del curso contando sobre los principales detalles de esta."
-                categories={["Categoría 1", "Categoría 2"]}
-                credits={3}
-                variant="small"
-                variantStyle="dashed"
-                isEditable = {true}
+        <section className="flex flex-col justify-center pl-40 pr-40 bg-white">
+            <div className="flex">
+                <div >
+                    <h1 className="text-7xl font-calsans text-black leading-none mt-16">Desarrolla tu Talento</h1>
+                    <div className="flex flex-row gap-24 mb-8">
+                        <p className="text-xl text-black pt-4 w-3/5">Nos adaptamos a todos los tiempos y niveles de aventura. Conoce cómo puedes explorar nuestros mundos.</p>
+                        <TalentSearchBar></TalentSearchBar>
+                    </div>
 
-            />
-            <CourseCard
-                title="Card Curso"
-                description="Descripción del curso contando sobre los principales detalles de esta."
-                categories={["Categoría 1", "Categoría 2"]}
-                credits={3}
-                variant="small"
-            />
-            <CourseCard
-                title="Card Curso"
-                description="Descripción del curso contando sobre los principales detalles de esta."
-                categories={["Categoría 1", "Categoría 2"]}
-                credits={3}
-                variant="small"
-            />
+                </div>
 
             </div>
-            
-            
-            <div className="flex flex-row gap-[1rem]">
-            <CourseCard
-                title="Card Certificación"
-                description="Descripción de la certificación contando sobre los principales detalles de esta."
-                categories={["Categoría 1", "Categoría 2"]}
-                credits={9}
-                variant="medium"
-            />
-            <CourseCard
-                title="Card Curso"
-                description="Descripción del curso contando sobre los principales detalles de esta."
-                categories={["Categoría 1", "Categoría 2"]}
-                credits={3}
-                variant="small"
-            />
-            </div>
-           
+            <div className="flex flex-row justify-center gap-6 bg-white">
+                <TalentCycleComponent></TalentCycleComponent>
+                <div className="flex flex-col gap-6 bg-white">
+                    <div className="flex flex-row gap-[1rem]">
+                        <CourseCard
+                            title="Card Curso"
+                            description="Descripción del curso contando sobre los principales detalles de esta."
+                            categories={["Categoría 1", "Categoría 2"]}
+                            credits={3}
+                            variant="small"
 
-            {/* Large Card */}
-            <CourseCard
-                title="Card Especialización"
-                description="Descripción de la especialización contando sobre los principales detalles de esta."
-                categories={["Categoría 1", "Categoría 2"]}
-                credits={15}
-                variant="large"
-            />
-            {/* XLarge Card */}
-            <PostGradCard
-                title="Card Doctorado"
-                description="Descripción del doctorado contando sobre los principales detalles de esta."
-                categories={["Categoría 1", "Categoría 2"]}
-                credits={21}
-            />
-        </div>
+
+                        />
+                        <CourseCard
+                            title="Card Curso"
+                            description="Descripción del curso contando sobre los principales detalles de esta."
+                            categories={["Categoría 1", "Categoría 2"]}
+                            credits={3}
+                            variant="small"
+                        />
+                        <CourseCard
+                            title="Card Curso"
+                            description="Descripción del curso contando sobre los principales detalles de esta."
+                            categories={["Categoría 1", "Categoría 2"]}
+                            credits={3}
+                            variant="small"
+                        />
+
+                    </div>
+
+
+                    <div className="flex flex-row gap-[1rem]">
+                        <CourseCard
+                            title="Card Certificación"
+                            description="Descripción de la certificación contando sobre los principales detalles de esta."
+                            categories={["Categoría 1", "Categoría 2"]}
+                            credits={9}
+                            variant="medium"
+                        />
+                        <CourseCard
+                            title="Card Curso"
+                            description="Descripción del curso contando sobre los principales detalles de esta."
+                            categories={["Categoría 1", "Categoría 2"]}
+                            credits={3}
+                            variant="small"
+                        />
+                    </div>
+
+
+                    {/* Large Card */}
+                    <CourseCard
+                        title="Card Especialización"
+                        description="Descripción de la especialización contando sobre los principales detalles de esta."
+                        categories={["Categoría 1", "Categoría 2"]}
+                        credits={15}
+                        variant="large"
+                    />
+
+                </div>
+            </div>
+        </section>
     );
 };
 
