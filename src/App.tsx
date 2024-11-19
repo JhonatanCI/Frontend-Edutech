46401024e0647f08ff58f884d776c26a52226d80
JCamiloRamirezTabares <juancaram21@gmail.com>
2024-11-19 12:43:25 -0500

    feat: add Home page - set Home as the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import NavBar from './components/NavBar';
-import HeroSection from './components/HeroSection';
-import WorldsSection from './components/WorldsSection';
-import ProgramSection from './components/ProgramsSection';
-import AdventureSection from './components/AdventureSection';
-
-
+import { Home } from "./pages/Home";
 
 function App() {
-
-
   return (
     <>
-      <NavBar />
-      <HeroSection/>
-      <WorldsSection/>
-      <ProgramSection />
-      <AdventureSection/>
-      
+      <Home/>
     </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import NavBar from "../components/NavBar"
+import HeroSection from "../components/HeroSection"
+import WorldsSection from "../components/WorldsSection"
+import ProgramSection from "../components/ProgramsSection"
+import AdventureSection from "../components/AdventureSection"
+
+export const Home = () => {
+    return (
+        <>
+            <NavBar />
+            <HeroSection />
+            <WorldsSection />
+            <ProgramSection />
+            <AdventureSection />
+        </>
+    )
+}
