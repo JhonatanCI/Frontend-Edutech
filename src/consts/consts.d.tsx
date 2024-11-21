124e8d34e15cf40a958cbd13345fec7548c6f69a
JCamiloRamirezTabares <juancaram21@gmail.com>
2024-11-21 07:54:28 -0500

    feat: add program images - added more images

--- src/consts/consts.d.tsx
+++ src/consts/consts.d.tsx
@@ -10,16 +10,21 @@ import pmMaster from "../assets/programs_assets/project_management_master.svg"
 import cbMaster from "../assets/programs_assets/cibersecurity_master.svg"
 import dsMaster from "../assets/programs_assets/data_science_master.svg"
 import deMaster from "../assets/programs_assets/digital_experiences_master.svg"
+import aiPHD from "../assets/programs_assets/AIPHD.svg"
+import pfCert from  "../assets/programs_assets/project_formulation_certif.svg"
+import tiInnovMaster from "../assets/programs_assets/tinnovation_master.svg"
+import aiAppliedMaster from "../assets/programs_assets/aplied_ai_master.svg"
+
 
 export const defaultPrograms: Program[] = [
     {
         id: "3e6bdc69-7f79-4545-a1e6-dcc65cd0f6a1",
-        name: "Maestría en Gerencia de Proyectos",
+        name: "Maestría en Ciencias de la Computación",
         description: "Descripción del programa, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
     },
     {
         id: "6599605e-cb4f-498e-82cd-79b2941bf513",
-        name: "Maestría en Ciencia de Datos",
+        name: "Maestría en Innovación Tecnologica",
         description: "Descripción del programa, contando sobre los principales detalles de esta. Máximo unas 3 líneas."
     },
     {
@@ -70,15 +75,11 @@ export const worldsImages = {
 
 export const programsImages = {
     "Maestría en Gerencia de Proyectos": pmMaster,
-    "Certificación en Formulación y Coordinación de Proyectos": aiWorld,
+    "Certificación en Formulación y Coordinación de Proyectos": pfCert,
     "Maestría en Ciencia de Datos": dsMaster,
     "Especialización en Ciberseguridad": cbMaster,
-    "Maestría en Inteligencia Artificial Aplicada": aiWorld,
+    "Maestría en Inteligencia Artificial Aplicada": aiAppliedMaster,
     "Certificación en Experiencias Digitales": deMaster,
-    "Maestría en Transformación Digital": aiWorld,
-    "Especialización en Gestión de TI": aiWorld,
-    "Maestría en Innovación Tecnológica": aiWorld,
-    "Certificación en Gestión Ágil de Proyectos": aiWorld,
-    "Doctorado en Transformación Digital y Estrategias Empresariales": aiWorld,
-    "Doctorado en Inteligencia Artificial y Ciencia de Datos": aiWorld,
+    "Maestría en Innovación Tecnológica": tiInnovMaster,
+    "Doctorado en Inteligencia Artificial y Ciencia de Datos": aiPHD,
 }
