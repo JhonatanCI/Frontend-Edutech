0f30c8213e93a2d135bcbc8d4510af20569d58df
JCamiloRamirezTabares <juancaram21@gmail.com>
2024-12-08 22:46:28 -0500

    feat: add LearningPath and KeepLearning sections - added sections to display programs related to the currently displayed program

--- src/components/Home/ProgramCard.tsx
+++ src/components/Home/ProgramCard.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface CardProps {
   image: string;
   title: string;
   description: string;
   buttonText: string;
+  path?: string;
 }
 
-const ProgramCard: React.FC<CardProps> = ({ image, title, description, buttonText }) => {
+const ProgramCard: React.FC<CardProps> = ({ image, title, description, buttonText, path }) => {
+  
+  const navigateTo = useNavigate()
+
+  const handleClick = () => {
+      path && navigateTo(path)
+  }
+  
   return (
     <div className="w-96 bg-white border border-textGray text-black rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
       {/* Card Image */}
@@ -20,7 +29,7 @@ const ProgramCard: React.FC<CardProps> = ({ image, title, description, buttonTex
       <div className="p-6 text-left">
         <h3 className="text-3xl font-calsans text-black mb-3">{title}</h3>
         <p className="text-black mb-6">{description}</p>
-        <button className="bg-white flex items-center text-black font-semibold hover:underline">
+        <button className="bg-white flex items-center text-black font-semibold hover:underline" onClick={handleClick}>
           {buttonText}
           <span className="ml-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5">
