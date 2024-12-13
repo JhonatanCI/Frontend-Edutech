194ef51f886f019484e019f24788db371f9fef4e
JCamiloRamirezTabares <juancaram21@gmail.com>
2024-12-12 21:57:13 -0500

    feat: add course swap functionality - implemented course swapping based on flexibility type (CONDICIONADO or FLEXIBLE)

--- src/services/search.ts
+++ src/services/search.ts
@@ -3,16 +3,16 @@ import { API } from "../config/axios"
 
 export const getGeneralResults = async() => {
     try {
-        const response = await API.get("/search")
+        const response = await API.get("/search/general")
         return response.data
     } catch (error) {
         throw error
     }
 }
 
-export const getResults = async(input: string) => {
+export const getResults = async(query: string) => {
     try {
-        const response = await API.get(`/search?worldName=${input}`)
+        const response = await API.get(`/search?query=${query}`)
         return response.data
     } catch (error) {
         throw error
